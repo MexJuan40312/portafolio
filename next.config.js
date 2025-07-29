d81cd236ad8f2aca738f3b90130a9f7b60002e61
MexJuan40312 <robertukisimon@hotmail.com>
2025-07-28 23:02:24 -0600

    Solución

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 // next.config.js
-const path = require("path");
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    config.resolve.alias["@"         ] = path.resolve(__dirname, "app");
-    config.resolve.alias["@components"] = path.resolve(__dirname, "app/components");
-    config.resolve.alias["@types"    ] = path.resolve(__dirname, "app/types");
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app');
+    config.resolve.alias['@components'] = path.resolve(__dirname, 'app/components');
+    config.resolve.alias['@types'] = path.resolve(__dirname, 'app/types');
     return config;
   },
 };
